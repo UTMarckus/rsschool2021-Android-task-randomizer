2316b114a86e496d1169aa402f52333d57ec50ff
T. Ustiugov <utmarckus@gmail.com>
2025-08-02 11:24:59 +0300

    refactor: update gradle and refactor MainActivity to kotlin

--- app/src/main/java/com/rsschool/android2021/MainActivity.kt
+++ app/src/main/java/com/rsschool/android2021/MainActivity.kt
@@ -1,29 +1,24 @@
-package com.rsschool.android2021;
+package com.rsschool.android2021
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.rsschool.android2021.FirstFragment.Companion.newInstance
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        openFirstFragment(0);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
-    private void openFirstFragment(int previousNumber) {
-        final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
-        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, firstFragment);
+    private fun openFirstFragment(previousNumber: Int) {
+        val firstFragment: Fragment = newInstance(previousNumber)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, firstFragment)
         // TODO: invoke function which apply changes of the transaction
     }
 
-    private void openSecondFragment(int min, int max) {
+    private fun openSecondFragment(min: Int, max: Int) {
         // TODO: implement it
     }
 }
